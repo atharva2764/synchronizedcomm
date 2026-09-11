a8c9e1e721c9e3350fa2311c2d1d5107640662b9
atharva2764 <atharva.rananaware@gmail.com>
2026-09-11 15:59:03 +0530

    Static Routing

---
+++ ecom-api-gateway/src/test/java/com/example/ecomapigateway/EcomApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecomapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
